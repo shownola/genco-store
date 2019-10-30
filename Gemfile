f320744c706dfbbaa490a6d4a87241d62e1c2ae6
shownola <swasieko@gmail.com>
2019-10-29 21:25:18 -0700

    Add bootstrap animations and start styling of home page

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
+gem 'animate-css-rails'
+gem "font-awesome-rails"
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'features', to: 'pages#features'
   get 'contact', to: 'pages#contact'
+
+  get 'portfolio', to: 'pages#portfolio'
+  get 'pricing', to: 'pages#pricing'
+  get 'team', to: 'pages#team'
+  get 'skills', to: 'pages#skills'
+  get 'clients', to: 'pages#clients'
 end
